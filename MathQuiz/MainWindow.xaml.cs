25e5eb512acfe167299abe422cdc6f51922523d7
unknown <abraidwood@alex-PC.KASSLAW.com>
2014-10-17 09:30:28 -0400

    Added UI and wired up keypad

--- MathQuiz/MainWindow.xaml.cs
+++ MathQuiz/MainWindow.xaml.cs
@@ -23,5 +23,52 @@ namespace MathQuiz
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            switch (button.Name)
+            {
+                case "OneButton":
+                    updateNewBoxText("1");
+                    break;
+                case "TwoButton":
+                    updateNewBoxText("2");
+                    break;
+                case "ThreeButton":
+                    updateNewBoxText("3");
+                    break;
+                case "FourButton":
+                    updateNewBoxText("4");
+                    break;
+                case "FiveButton":
+                    updateNewBoxText("5");
+                    break;
+                case "SixButton":
+                    updateNewBoxText("6");
+                    break;
+                case "SevenButton":
+                    updateNewBoxText("7");
+                    break;
+                case "EightButton":
+                    updateNewBoxText("8");
+                    break;
+                case "NineButton":
+                    updateNewBoxText("9");
+                    break;
+                case "ZeroButton":
+                    updateNewBoxText("0");
+                    break;
+            }
+        }
+
+        public void updateNewBoxText(string number)
+        {
+            if (!(number.Equals("0") && NumberBox.Text.Equals("0")))
+            {
+                NumberBox.Text = string.Concat(NumberBox.Text, number);
+            }
+        }
     }
 }
